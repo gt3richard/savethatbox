e11f82b2fb6919b3c33a0e0e41d391890f2d3275
Richard Ali <richardali@richards-mbp.lan>
2020-07-10 01:13:43 -0700

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential', 
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
+    'prettier',
+    'prettier/vue'
   ],
   // required to lint *.vue files
   plugins: [
